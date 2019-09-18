e6e3427d15abe86ece9225b1131ec5fd3785420c
Tiryoh <tiryoh@gmail.com>
2019-09-18 19:41:00 +0900

    Add install.sh

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ros_melodic_raspizero
 ROS Melodic Morenia for Raspberry Pi Zero/Zero W
+
+ROS Kinetic Kame: https://github.com/nomumu/Kinetic4RPiZero
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu
+
+sudo apt install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential git curl
+sudo rosdep init
+rosdep update
+sudo mkdir -p /usr/src/ros/melodic
+sudo chown -R $USER:$USER /usr/src/ros
+cd /usr/src/ros/melodic
+rosinstall_generator robot --rosdistro melodic --deps --tar > melodic-robot.rosinstall
+wstool init -j1 src melodic-robot.rosinstall
+rosdep install --from-paths src --ignore-src --rosdistro melodic -y
+sudo mkdir -p /opt/ros/melodic
+echo 'sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release  --install-space /opt/ros/melodic -j1' > install.sh
+bash ./install.sh
