a7f73f36df92e1656ec4be776d2016c6f13ea91e
MELNYK <artemmelnyk@gmail.com>
2019-12-02 07:39:07 +0100

    Add bashrc settings

--- install.sh
+++ install.sh
@@ -14,3 +14,5 @@ rosdep install --from-paths src --ignore-src --rosdistro melodic -y
 sudo mkdir -p /opt/ros/melodic
 echo 'sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release  --install-space /opt/ros/melodic -j1' > install.sh
 bash ./install.sh
+grep -F "source /opt/ros/melodic/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
